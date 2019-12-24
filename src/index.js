ac185841933f642cab064278da17edc164b4c438
Jin-pyo <48873560+Jin-pyo@users.noreply.github.com>
2019-12-24 11:04:10 -0500

    redux start

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Create a new componetn. this component should produce
+// some HTML
+const App = function(){
+    return <div>Hi</div>;
+}
+
+
+// Take this compoent's generated HTML and put it
+// ont the page (in the DOM)
+
