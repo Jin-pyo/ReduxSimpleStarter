92ed4d3197a9c42729d6ea4bf33f2c6c0d188d63
Jin-pyo <48873560+Jin-pyo@users.noreply.github.com>
2019-12-25 11:11:37 -0500

    Update video_list_item.js

---
+++ src/components/video_list_item.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const VideoListItem=({video})=>{
+    //const video =props.video; 위에 파라미터를 
+    //props말고 바로 video로 써서 이걸안써줘도됨
+    const imageUrl=video.snippet.thumbnails.default.url;
+
+    return (
+    <li className="list-group-item">
+        <div className="video=list media">
+            <div className="media-left">
+                <img className="media-object" src={imageUrl} />
+            </div>
+
+            <div className="media-body">
+                <div className="media-heading">{video.snippet.title}</div>
+            </div>
+        </div>
+    </li>
+    );
+};
+
+export default VideoListItem;
